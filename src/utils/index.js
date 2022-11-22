21d76c0393b8478b4426e70706fca0537841eb7c
Your Name <you@example.com>
2022-11-22 17:52:52 +0800

    .

--- src/utils/index.js
+++ src/utils/index.js
@@ -297,15 +297,15 @@ export function deepClone(source) {
   if (!source && typeof source !== 'object') {
     throw new Error('error arguments', 'deepClone')
   }
-  const targetObj = source.constructor === Array ? [] : {}
+  const targetObject = source.constructor === Array ? [] : {}
   Object.keys(source).forEach(keys => {
     if (source[keys] && typeof source[keys] === 'object') {
-      targetObj[keys] = deepClone(source[keys])
+      targetObject[keys] = deepClone(source[keys])
     } else {
-      targetObj[keys] = source[keys]
+      targetObject[keys] = source[keys]
     }
   })
-  return targetObj
+  return targetObject
 }
 
 /**
